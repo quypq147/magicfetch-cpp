497f5287848d9f1586d0ff1d5f0d188aedfe9400
quypq147 <phanngocquy1472004@gmail.com>
2024-09-30 23:38:04 +0700

    just update

--- magicfetch/Madoart.h
+++ magicfetch/Madoart.h
@@ -20,3 +20,25 @@ const std::wstring win10art17 = L"`'ccllllllllll  lllllllllllllllllll";
 const std::wstring win10art18 = LR"(      `' \\*::  :ccllllllllllllllll)"; // <---- this line had to be a raw string literal because of the backslashes
 const std::wstring win10art19 = L"                       ````''*::cll";         // which the compiler assumes are escape chars (like \n), but if you're
 const std::wstring win10art20 = L"                                 ``";
+
+
+const std::wstring win11art01 = L"                                ..,";
+const std::wstring win11art02 = L"                                ..,";
+const std::wstring win11art03 = L"                                ..,";
+const std::wstring win11art04 = L"                                ..,";
+const std::wstring win11art05 = L"                                ..,";
+const std::wstring win11art06 = L"                                ..,";
+const std::wstring win11art07 = L"                                ..,";
+const std::wstring win11art08 = L"                                ..,";
+const std::wstring win11art09 = L"                                ..,";
+const std::wstring win11art10 = L"                                ..,";
+const std::wstring win11art11 = L"                                ..,";
+const std::wstring win11art12 = L"                                ..,";
+const std::wstring win11art13 = L"                                ..,";
+const std::wstring win11art14 = L"                                ..,";
+const std::wstring win11art15 = L"                                ..,";
+const std::wstring win11art16 = L"                                ..,";
+const std::wstring win11art17 = L"                                ..,";
+const std::wstring win11art18 = L"                                ..,";
+const std::wstring win11art19 = L"                                ..,";
+const std::wstring win11art20 = L"                                ..,";
